51a8db3fa5ac2658cf431a62ff6f9d9afe5c8e8a
Sai kiran Erram <saikiran.erram11@gmail.com>
2026-06-24 14:45:24 +0530

    Extend calculator to handle three numbers

--- cal.py
+++ cal.py
@@ -1,16 +1,16 @@
 num1 = float(input("Enter first number: "))
 operator = input("Enter operator (+, -, *, /): ")
 num2 = float(input("Enter second number: "))
-
+num3 = float(input("Enter third number: "))
 if operator == "+":
     print("Result =", num1 + num2 + num3)
 elif operator == "-":
-    print("Result =", num1 - num2)
+    print("Result =", num1 - num2 - num3)
 elif operator == "*":
-    print("Result =", num1 * num2)
+    print("Result =", num1 * num2 * num3)
 elif operator == "/":
     if num2 != 0:
-        print("Result =", num1 / num2)
+        print("Result =", num1 / num2 / num3)
     else:
         print("Cannot divide by zero")
 else:
